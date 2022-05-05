146a8d96eb74ced808fe45670c69756a66592882
EthanRockss <ethanrocksssss@gmail.com>
2022-05-05 14:24:38 -0600

    able to load and unload cogs and it saves upon bot restart

--- Cogs/cogManager.py
+++ Cogs/cogManager.py
@@ -10,16 +10,63 @@ class cogManager(commands.Cog):
     def __init__(self, bot:commands.Bot):
         self.bot = bot
 
-    async def getEnabledCogs():
-        pass
+    @commands.is_owner()
+    @commands.command(name = "load", description="load cog")
+    async def load(self, ctx:commands.Context, cog:str=None):
+        if cog == None or cog not in os.listdir("Cogs"):
+            await ctx.send("Dude what cog do you want me to load.")
+            return
+        if not cog.endswith(".py"):
+            cog.append(".py")
+        try:
+            self.bot.load_extension(f"Cogs.{cog[:-3]}")
+            with open("cogConfig.json", "r") as config:
+                data = json.load(config)
+                data["Cogs"][cog] = True
+                config.close()
+            with open("cogConfig.json", "w") as config:
+                json.dump(data, config, ensure_ascii=False, indent=4)
+        except commands.errors.ExtensionAlreadyLoaded:
+            await ctx.send("that cog is already loaded bro.")
 
-try:
-    with open("Cogs/Configs/cogManager.json", "r") as config: 
-        data = json.load(config)
-        enabledCogs = data[]
-    config.close()
-except OSError as e:
-    ConfigCreation.createConfig("cogManager", DEFAULT_CONFIG, True)
+    @commands.is_owner()
+    @commands.command(name = "unload", description="unload cog")
+    async def unload(self, ctx:commands.Context, cog:str=None):
+        if cog == None or cog not in os.listdir("Cogs"):
+            await ctx.send("Dude what cog do you want me to unload.")
+            return
+        if not cog.endswith(".py"):
+            cog.append(".py")
+        try:
+            self.bot.unload_extension(f"Cogs.{cog[:-3]}")
+            with open("cogConfig.json", "r") as config:
+                data = json.load(config)
+                data["Cogs"][cog] = False
+                config.close()
+            with open("cogConfig.json", "w") as config:
+                json.dump(data, config, ensure_ascii=False, indent=4)
+        except commands.errors.ExtensionNotLoaded:
+            await ctx.send("that cog is already unloaded bro.")
+
+    @commands.is_owner()
+    @commands.command(name = "cogs", description="see list of cogs")
+    async def cogs(self, ctx:commands.Context):
+        cogs = getAllCogs()
+        if not cogs:
+            await ctx.send("there is no available cogs")
+            return
+        message = ", ".join(cogs)
+        await ctx.send("```{}```".format(message))
+
+
+for i in range(5):
+    try:
+        with open("cogConfig.json", "r") as config: 
+            data = json.load(config)
+            enabledCogs = data["Cogs"]
+        config.close()
+    except OSError as e:
+        ConfigCreation.createConfig("cogConfig", DEFAULT_CONFIG, False)
 
 def getAllCogs():
     cogs = []
--- main.py
+++ main.py
@@ -36,51 +36,36 @@ class BotCore(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		self._last_member = None
-
-	@commands.is_owner()
-	@commands.command(name = "load", description="load cog")
-	async def load(self, ctx:commands.Context, cog:str=None):
-		if not cog.endswith(".py"):
-			cog.append(".py")
-		if cog == None or cog not in os.listdir("Cogs"):
-			await ctx.send("Dude what cog do you want me to load.")
-		else:
-			try:
-				bot.load_extension(f"Cogs.{cog}")
-				with open("cogConfig.json", "r") as config:
-					data = json.load(config)
-					data["Cogs"][cog] = True
-					json.dump(data, config, ensure_ascii=False, indent=4)
-			except commands.errors.ExtensionNotFound:
-				await ctx.send("that cog is already loaded bro.")
 			
-				
-		
-
 # Intents
 intents = discord.Intents.all()
 # The bot
 bot = commands.Bot(prefix, intents = intents, owner_id = owner_id)
 
 # Load cogs
-try:
-	with open("cogConfig.json", "r") as config: 
-		data = json.load(config)
-		cogs = data["Cogs"]
-		config.close()
-	failedCogs = []
-	for i in cogs:
-		try:
-			bot.load_extension(f"Cogs.{i[:-3]}")
-		except commands.errors.ExtensionNotFound:
-			if i not in os.listdir("Cogs"):
-				failedCogs.append(i)
-	for i in failedCogs:
-		cogs.pop(i, None)
-		with open("cogConfig.json", "r") as config:
-			json.dump(cogs, config, ensure_ascii=False, indent=4)
-except OSError as e:
-	ConfigCreation.createConfig("cogConfig", COG_CONFIG)
+for i in range(5):
+	try:
+		with open("cogConfig.json", "r") as config: 
+			data = json.load(config)
+			cogs = data["Cogs"]
+			config.close()
+		failedCogs = []
+		for i in cogs:
+			try:
+				bot.load_extension(f"Cogs.{i[:-3]}")
+			except commands.errors.ExtensionNotFound:
+				if i not in os.listdir("Cogs"):
+					failedCogs.append(i)
+		for i in failedCogs:
+			cogs.pop(i, None)
+			with open("cogConfig.json", "r") as config:
+				json.dump(cogs, config, ensure_ascii=False, indent=4)
+		break
+	except OSError as e:
+		ConfigCreation.createConfig("cogConfig", COG_CONFIG)
+	except json.decoder.JSONDecodeError as e:
+		with open("cogConfig.json", "w") as config:
+			json.dump(COG_CONFIG, config, ensure_ascii=False, indent=4)
 
 @bot.event
 async def on_ready():
