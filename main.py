0d01b0cad66de8518076118d4eb45ed64c2201c9
EthanRockss <ethanrocksssss@gmail.com>
2023-01-25 15:53:16 -0700

    add more things to config, fix status command, clean up stop command and finally add the players command

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import os
 from discord import app_commands
 from googleapiclient import discovery
+from rcon.source import Client as rcon
 
 PRIMARY_GUILD = discord.Object(id=952505243072094258)
 
@@ -11,7 +12,12 @@ DEFAULT_CONFIG = {
     "owner_id": 0,
 	"project_id": "",
 	"zone": "",
-	"instance_name": ""
+	"instance_name": "",
+	"server_ip": "",
+	"rcon": {
+		"password": "",
+		"port": ""
+	}
 }
 
 GAME_IP = "34.106.100.58:7777"
@@ -27,7 +33,9 @@ with open("configuration.json", "r") as config:
 		owner_id = data["owner_id"]
 		project = data["project_id"]
 		zone = data["zone"]
+		server_ip = data["server_ip"]
 		instance = data["instance_name"]
+		rconconf = data["rcon"]
 		os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = data["GACPath"]
 	except:
 		print("There is a problem with your configuration file\nMake sure to include a token and your discord id.")
@@ -75,6 +83,8 @@ async def status(interaction: discord.Interaction):
 		await interaction.response.send_message(f"The server is running.\nConnect via the Mordhau server browser search for `flom training grounds`.\nPassword is nfa123")
 	elif status == 3:
 		await interaction.response.send_message("Server is currently shutting down.")
+	else:
+		await interaction.response.send_message("something went wrong")
 
 @client.tree.command(name="start", description="start the game instance")
 async def start(interaction: discord.Interaction):
@@ -90,8 +100,21 @@ async def start(interaction: discord.Interaction):
 @app_commands.check(checkifbased)
 @client.tree.command(name="stop", description="stop the game server instance")
 async def stop(interaction: discord.Interaction):
-	request = service.instances().stop(project=project, zone=zone, instance=instance)
-	response = request.execute()
+	service.instances().stop(project=project, zone=zone, instance=instance).execute()
 	await interaction.response.send_message("Server shutting down.")
+
+@client.tree.command(name="players", description="get list of players on the server")
+async def players(interaction: discord.Interaction):
+	with rcon(server_ip, rconconf["port"], passwd=rconconf["password"]) as client:
+		response = client.run('playerlist')
+	players = int
+
+	if response.startswith("There are currently no players present"):
+		players = 0
+	else:
+		players = response.count("\n")
+
+	await interaction.response.send_message(f"There are currently {players} players online.")
+
 
 client.run(token)
