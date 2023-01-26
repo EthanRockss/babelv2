d05026115c9ef3f9005f59b8a078b00a1951db84
EthanRockss <ethanrocksssss@gmail.com>
2023-01-26 00:30:07 -0700

    adds autoshutdown script that will shut down the server meant to be assigned to a cronjob

---
+++ autoshutdown.py
@@ -0,0 +1,55 @@
+import pickle
+import json
+from os import environ
+from googleapiclient import discovery
+from rcon.source import Client as rcon
+
+with open("configuration.json", "r") as config: 
+	data = json.load(config)
+	try:
+		project = data["project_id"]
+		zone = data["zone"]
+		instance = data["instance_name"]
+		environ["GOOGLE_APPLICATION_CREDENTIALS"] = data["gac_path"]
+		rconconf = data["rcon"]
+	except:
+		print("failed to load config please run main.py before running this script.")
+		exit()
+
+port = rconconf["port"]
+password = rconconf["password"]
+
+service = discovery.build("compute", "v1")
+try:
+	server_ip = service.instances().get(project=project, zone=zone, instance=instance).execute()["networkInterfaces"][0]["accessConfigs"][0]["natIP"]
+except KeyError:
+	print("Server seems to be offline or unreachable")
+	exit()
+	
+try:
+	with open("prev_check.pk", "rb") as fi:
+			prev_check = pickle.load(fi)
+			fi.close()
+except:
+	with open("prev_check.pk", "w+b") as fi:
+		pickle.dump(False, fi)
+	fi.close()
+	prev_check = False
+
+with rcon(server_ip, port, passwd=password) as connection:
+		response = connection.run('playerlist')
+		if response.count("\n") == 0:
+			zero_players = True
+			print("there are zero players")
+		else:
+			zero_players = False
+			print("there are players")
+		
+
+if prev_check == True and zero_players == True:
+	print("shutting instance down")
+	service.instances().stop(project=project, zone=zone, instance=instance).execute()
+else:
+	with open("prev_check.pk", "wb") as fi:
+		pickle.dump(zero_players, fi)
+	fi.close()
