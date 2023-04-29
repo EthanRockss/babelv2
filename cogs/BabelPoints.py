b17a3bff7582c708a18e7ed959bc54eae36633fc
EthanRockss <ethanrocksssss@gmail.com>
2023-04-29 12:05:40 -0600

    added command to add custom emoji to settings

--- cogs/BabelPoints.py
+++ cogs/BabelPoints.py
@@ -1,30 +1,30 @@
 import discord
-import os
 import sqlite3
 from random import randint
 from discord.ext import commands
-from discord.ext.commands import Context
+from discord import app_commands
 
 con = sqlite3.connect("cogs/cogsettings/babpoints.db")
 cur = con.cursor()
 cur.execute('''CREATE TABLE IF NOT EXISTS babelpoints (guildid, userid, points)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS babelpointsconfig (guildid, emoji)''')
 
 class BabelPoints(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.Cog.listener("on_guild_channel_pins_update")
-    async def channel_pins_update(self, channel: discord.TextChannel, last_pin):
-        print(channel.id)
-        for i in await channel.pins():
-            print(i.id)
-        #  cur.execute('''SELECT * FROM babelpoints WHERE guildid=?''', (guildid))
-        #  if cur.fetchone() != None:
-        #       pass
-        #  else:
-        #       cur.execute('''INSERT INTO babelpoints (guildid, userid, points) VALUES (?, ?, ?)''', (guildid, userid, 1))
-    
+    @app_commands.guild_only()
+    @app_commands.command()
+    @app_commands.checks.has_permissions(administrator=True)
+    async def bpset(self, interaction: discord.Interaction, custom_emoji: discord.Emoji):
+        await interaction.response.defer(ephemeral=True)
+        guildid = interaction.guild.id
+        cur.execute('''SELECT * FROM babelpointsconfig WHERE guildid=?''', (guildid,))
+        if cur.fetchone() != None:
+             cur.execute('''UPDATE babelpointsconfig SET emoji=? WHERE guildid=?''', (custom_emoji, guildid))
+        else:
+             cur.execute('''INSERT INTO babelpointsconfig (guildid, emoji) VALUES (?, ?)''', (guildid, custom_emoji))
 
 async def setup(bot: commands.Bot) -> None:
 	await bot.add_cog(BabelPoints(bot))
